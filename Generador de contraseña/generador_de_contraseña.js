907e4bb07fbea43b8f5e31b7475ff6f6754b9fea
unknown <pabloespinosa888@gmail.com>
2026-03-27 21:02:12 -0300

    Se inicio el generador de contraseña

---
+++ Generador de contraseña/generador_de_contraseña.js
@@ -0,0 +1,14 @@
+//Programa para generar contraseña
+
+const tamañoDeContraseña = document.getElementById("tamañoDeConstraseña");
+const incluirMinusculas = document.getElementById("incluirMinusculas");
+const inclurMayusculas = document.getElementById("inclurMayusculas");
+const inclurNumeros = document.getElementById("inclurNumeros");
+const inclurSimbolos = document.getElementById("inclurSimbolos");
+const contraseñaCreada = document.getElementById("contraseñaCreada");
+
+function generarContraseña(){
+
+    
+
+}
