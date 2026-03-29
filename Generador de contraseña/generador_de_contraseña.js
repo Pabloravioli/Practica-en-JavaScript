8c79542c4449d4d797c219ef2b8321bc910d7b57
unknown <pabloespinosa888@gmail.com>
2026-03-29 00:26:52 -0300

    Se implemento una validacion

--- Generador de contraseña/generador_de_contraseña.js
+++ Generador de contraseña/generador_de_contraseña.js
@@ -1,14 +1,26 @@
 //Programa para generar contraseña
 
-const tamañoDeContraseña = document.getElementById("tamañoDeConstraseña");
+const tamañoDeContraseña = document.getElementById("tamañoDeContraseña");
 const incluirMinusculas = document.getElementById("incluirMinusculas");
-const inclurMayusculas = document.getElementById("inclurMayusculas");
-const inclurNumeros = document.getElementById("inclurNumeros");
-const inclurSimbolos = document.getElementById("inclurSimbolos");
+const incluirMayusculas = document.getElementById("inclurMayusculas");
+const incluirNumeros = document.getElementById("inclurNumeros");
+const incluirSimbolos = document.getElementById("inclurSimbolos");
 const contraseñaCreada = document.getElementById("contraseñaCreada");
+
+ 
 
 function generarContraseña(){
 
-    
+    let resultado;
 
+    const tamaño = Number(tamañoDeContraseña.value);
+
+    if (tamaño < 4){
+
+        resultado = "Longitud invalida" ;
+        contraseñaCreada.textContent = resultado;
+    }
+   
+
+ 
 }
