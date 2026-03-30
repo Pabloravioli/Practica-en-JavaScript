d0fa391302ff26691cf46f42277b1677c6b6372e
unknown <pabloespinosa888@gmail.com>
2026-03-30 19:01:22 -0300

    Se corrigio la validacion

--- Generador de contraseña/generador_de_contraseña.js
+++ Generador de contraseña/generador_de_contraseña.js
@@ -11,15 +11,44 @@ const contraseñaCreada = document.getElementById("contraseñaCreada");
 
 function generarContraseña(){
 
-    let resultado;
+    let resultado = "";
+    let caracteresPermitidos = "";
 
     const tamaño = Number(tamañoDeContraseña.value);
+
+    const minusculas = "abcdefghijklmnopqrstuvwxyz";
+    //const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   // const numeros = "1234567890";
+   // const simbolos ="! @ # $ % ^ & * ( ) - _ = +";
+
 
     if (tamaño < 4){
 
         resultado = "Longitud invalida" ;
         contraseñaCreada.textContent = resultado;
     }
+    
+    else{
+
+        caracteresPermitidos += incluirMinusculas.checked ? minusculas: "";
+        
+
+
+        
+        console.log(caracteresPermitidos);
+        for (let i=0;i<tamaño;i++){
+            const index = Math.floor(Math.random()*caracteresPermitidos.length);
+            resultado += caracteresPermitidos[index];
+            
+    
+    
+        }
+
+        contraseñaCreada.textContent = resultado;
+    }
+
+
+    
    
 
  
